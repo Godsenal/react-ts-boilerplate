e19c41699c242f60e74ea154b6c2ed3cf99d7261
Godsenal <tmqps78@gmail.com>
2018-07-25 21:55:34 +0900

    Fix intialState type

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -3,13 +3,16 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import rootReducer from '../reducers';
 import { RootState } from '../types';
 
-function configureStoreProd(initialState?: object | undefined) {
+function configureStoreProd(initialState?: object) {
   // Redux middlewares like thunks, saga
   // const middlewares = [
   //  *here*
   // ];
 
-  return createStore(rootReducer, initialState);
+  // use ! for telling compiler "this expression cannot be null or undefined here
+  // so don't complain about possibility of initialState being null or undefined"
+  // https://github.com/Microsoft/TypeScript/wiki/What's-new-in-TypeScript#non-null-assertion-operator
+  return createStore(rootReducer, initialState!);
   // if you applied middlewares above,
   // createStore(rootReducer, initialState, compose( applyMiddleware(...middlewares)));
 }
@@ -17,14 +20,14 @@ function configureStoreProd(initialState?: object | undefined) {
 /* Use redux-immutable-state-invariant
    which watct state's immutability
 */
-function configureStoreDev(initialState?: object | undefined) {
+function configureStoreDev(initialState?: object) {
   const middlewares = [
     reduxImmutableStateInvariant(),
     /* Redux middlewares like thunks */
   ];
 
   const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(rootReducer, initialState, composeEnhancers(
+  const store = createStore(rootReducer, initialState!, composeEnhancers(
     applyMiddleware(...middlewares)
   ));
 
