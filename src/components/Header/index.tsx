b7d55f3f82c59ab249dd5a912a14e0075fc3fb3e
Godsenal <tmqps78@gmail.com>
2018-08-15 22:42:35 +0900

    Fix bug

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const styles = require('./Header.scss');
 
 const Header: React.SFC = () => {
-    return (
-        <div className={styles.container}>
-            <span className={styles.link}>
-                <Link to="/">Go to Main</Link>
-            </span>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <span className={styles.link}>
+        <Link to="/">Go to Main</Link>
+      </span>
+    </div>
+  );
+};
 
 export default Header;
